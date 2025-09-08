f3289932e7fae3597788d9b38ec9b401f094bda8
SayanMagar2012 <639405@student.dmschools.org>
2025-09-08 15:15:33 -0500

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,264 @@
+// Here all versions are located, the same as app.js but this time its apps.js because all versions info is located in this one file
+// To add more versions, copy the versions.v1 and replace with versions.v2, and the same with others (it support more than 9 versions)
+
+// To learn how to use it correctly, visit https://incredirem.com/rmb
+
+versions.v1 = {
+  name: "Piège",
+  version: "v1",
+  date: "2025",
+  folder: "asset-v1/",
+  background: "game-bck@2x.jpg",
+  poloSprite: "polo-sprite.png",
+  poloHoverEffect: false,
+  ambience: "",
+  firstLoopDelay: 300,
+  looptime: 7111,
+  bpm: 135,
+  totalframe: 340,
+  nbpolo: 7,
+  maxrecloop: 24,
+  bonusloopA: true,
+  bonusendloopA: true,
+  _letEvents: true,
+  colBck: "#0a0d20",
+  col0: "#0D8A6C",
+  col1: "#06B187",
+  col2: "#0D8A6C",
+  col3: "#004938",
+  col4: "#001711",
+  pictoRowMax: 10,
+  animearray: [
+    {
+      name: "01_floral_shaman",
+      color: "277644",
+      uniqsnd: !1,
+    },
+    {
+      name: "02_macro_slate",
+      color: "277644",
+      uniqsnd: !0,
+    },
+    {
+      name: "03_chronus",
+      color: "277644",
+      uniqsnd: !0,
+    },
+    {
+      name: "04_technic_mudraker",
+      color: "277644",
+      uniqsnd: !1,
+    },
+    {
+      name: "05_halcyon_seeker",
+      color: "277644",
+      uniqsnd: !1,
+    },
+    {
+      name: "06_black_swan",
+      color: "8d2971",
+      uniqsnd: !1,
+    },
+    {
+      name: "07_strive",
+      color: "8d2971",
+      uniqsnd: !0,
+    },
+    {
+      name: "08_serpent_gods_vessel",
+      color: "8d2971",
+      uniqsnd: !1,
+    },
+    {
+      name: "09_black_floyd",
+      color: "8d2971",
+      uniqsnd: !1,
+    },
+    {
+      name: "10_electromagnet",
+      color: "8d2971",
+      uniqsnd: !1,
+    },
+    {
+      name: "11_dragonborn_warrior",
+      color: "d52441",
+      uniqsnd: !1,
+    },
+    {
+      name: "12_ancient_deity",
+      color: "d52441",
+      uniqsnd: !1,
+    },
+    {
+      name: "13_cosmic_knight",
+      color: "d52441",
+      uniqsnd: !1,
+    },
+    {
+      name: "14_space_sailor",
+      color: "d52441",
+      uniqsnd: !0,
+    },
+    {
+      name: "15_interdimensional_hero",
+      color: "d52441",
+      uniqsnd: !1,
+    },
+    {
+      name: "16_star_explorer",
+      color: "4974fe",
+      uniqsnd: !1,
+    },
+    {
+      name: "17_sail",
+      color: "4974fe",
+      uniqsnd: !0,
+    },
+    {
+      name: "18_prismatic",
+      color: "4974fe",
+      uniqsnd: !1,
+    },
+    {
+      name: "19_solomon",
+      color: "4974fe",
+      uniqsnd: !1,
+    },
+    {
+      name: "20_agatha",
+      color: "4974fe",
+      uniqsnd: !1,
+    },
+  ],
+  bonusarray: [],
+};
+
+versions.v2 = {
+  name: "Two Faces",
+  version: "v2",
+  date: "2025",
+  folder: "asset-v2/",
+  background: "game-bck@2x.jpg",
+  poloSprite: "polo-sprite.png",
+  poloHoverEffect: false,
+  ambience: "",
+  firstLoopDelay: 300,
+  looptime: 5647,
+  bpm: 170,
+  totalframe: 270,
+  nbpolo: 7,
+  maxrecloop: 24,
+  bonusloopA: true,
+  bonusendloopA: true,
+  _letEvents: true,
+  colBck: "#0f052a",
+  col0: "#000000",
+  col1: "#ffba42",
+  col2: "#000000",
+  col3: "#854f19",
+  col4: "#000000",
+  pictoRowMax: 10,
+  animearray: [
+    {
+      name: "01_atlanta",
+      color: "007fbb",
+      uniqsnd: !1,
+    },
+    {
+      name: "02_limez",
+      color: "007fbb",
+      uniqsnd: !1,
+    },
+    {
+      name: "03_foubreak",
+      color: "007fbb",
+      uniqsnd: !1,
+    },
+    {
+      name: "04_koukaklap",
+      color: "007fbb",
+      uniqsnd: !0,
+    },
+    {
+      name: "05_touku",
+      color: "007fbb",
+      uniqsnd: !0,
+    },
+    {
+      name: "06_bass",
+      color: "57697c",
+      uniqsnd: !1,
+    },
+    {
+      name: "07_monk",
+      color: "57697c",
+      uniqsnd: !1,
+    },
+    {
+      name: "08_doot",
+      color: "57697c",
+      uniqsnd: !0,
+    },
+    {
+      name: "09_soufle",
+      color: "57697c",
+      uniqsnd: !1,
+    },
+    {
+      name: "10_woop",
+      color: "57697c",
+      uniqsnd: !1,
+    },
+    {
+      name: "11_arpeg",
+      color: "ffd400",
+      uniqsnd: !1,
+    },
+    {
+      name: "12_dong",
+      color: "ffd400",
+      uniqsnd: !0,
+    },
+    {
+      name: "13_tiktok",
+      color: "ffd400",
+      uniqsnd: !0,
+    },
+    {
+      name: "14_ding",
+      color: "ffd400",
+      uniqsnd: !1,
+    },
+    {
+      name: "15_chor",
+      color: "ffd400",
+      uniqsnd: !1,
+    },
+    {
+      name: "16_goonthis",
+      color: "dc483d",
+      uniqsnd: !1,
+    },
+    {
+      name: "17_aaa",
+      color: "dc483d",
+      uniqsnd: !1,
+    },
+    {
+      name: "18_ou",
+      color: "dc483d",
+      uniqsnd: !1,
+    },
+    {
+      name: "19_twofaces",
+      color: "dc483d",
+      uniqsnd: !1,
+    },
+    {
+      name: "20_future",
+      color: "dc483d",
+      uniqsnd: !1,
+    },
+  ],
+  bonusarray: [],
+};
